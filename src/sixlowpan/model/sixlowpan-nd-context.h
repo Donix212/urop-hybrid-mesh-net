e23750e4507fafc5973cd8376a7dde188d8e8fae
Tommaso Pecorella <tommaso.pecorella@unifi.it>
2022-10-09 16:14:47 +0200

    First commit - needed a full rebase

--- src/sixlowpan/model/sixlowpan-nd-context.h
+++ src/sixlowpan/model/sixlowpan-nd-context.h
@@ -1,3 +1,4 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2015 Università di Firenze, Italy
  *
@@ -22,139 +23,139 @@
 #ifndef SIXLOW_ND_CONTEXT_H
 #define SIXLOW_ND_CONTEXT_H
 
-#include "ns3/ipv6-address.h"
-#include "ns3/nstime.h"
-#include "ns3/output-stream-wrapper.h"
-#include "ns3/simple-ref-count.h"
-
 #include <stdint.h>
+
+#include "ns3/nstime.h"
+#include "ns3/ipv6-address.h"
+#include "ns3/simple-ref-count.h"
+#include "ns3/output-stream-wrapper.h"
 
 namespace ns3
 {
 
 /**
- * @ingroup sixlowpan
- * @brief 6LoWPAN context container for 6LoWPAN ND.
+ * \ingroup sixlowpan
+ * \brief 6LoWPAN context container for 6LoWPAN ND.
  */
 class SixLowPanNdContext : public SimpleRefCount<SixLowPanNdContext>
 {
-  public:
-    /**
-     * @brief Constructor.
-     */
-    SixLowPanNdContext();
+public:
+  /**
+   * \brief Constructor.
+   */
+  SixLowPanNdContext ();
 
-    /**
-     * @brief Constructor.
-     * @param flagC compression flag
-     * @param cid context identifier ( 0 <= x <= 15)
-     * @param time valid lifetime of context
-     * @param context 6LoWPAN context advertised
-     */
-    SixLowPanNdContext(bool flagC, uint8_t cid, Time time, Ipv6Prefix context);
+  /**
+   * \brief Constructor.
+   * \param flagC compression flag
+   * \param cid context identifier ( 0 <= x <= 15)
+   * \param time valid lifetime of context
+   * \param context 6LoWPAN context advertised
+   */
+  SixLowPanNdContext (bool flagC, uint8_t cid, Time time, Ipv6Prefix context);
 
-    /**
-     * @brief Destructor.
-     */
-    ~SixLowPanNdContext();
+  /**
+   * \brief Destructor.
+   */
+  ~SixLowPanNdContext ();
 
-    /**
-     * @brief Get the context length.
-     * @return context length value
-     */
-    uint8_t GetContextLen() const;
+  /**
+   * \brief Get the context length.
+   * \return context length value
+   */
+  uint8_t GetContextLen () const;
 
-    /**
-     * @brief Is compression flag ?
-     * @return true if context is valid for use in compression, false otherwise
-     */
-    bool IsFlagC() const;
+  /**
+   * \brief Is compression flag ?
+   * \return true if context is valid for use in compression, false otherwise
+   */
+  bool IsFlagC () const;
 
-    /**
-     * @brief Set the compression flag.
-     * @param c the compression flag
-     */
-    void SetFlagC(bool c);
+  /**
+   * \brief Set the compression flag.
+   * \param c the compression flag
+   */
+  void SetFlagC (bool c);
 
-    /**
-     * @brief Get the context identifier.
-     * @return context identifier value
-     */
-    uint8_t GetCid() const;
+  /**
+   * \brief Get the context identifier.
+   * \return context identifier value
+   */
+  uint8_t GetCid () const;
 
-    /**
-     * @brief Set the context identifier.
-     * @param cid the context identifier value
-     */
-    void SetCid(uint8_t cid);
+  /**
+   * \brief Set the context identifier.
+   * \param cid the context identifier value
+   */
+  void SetCid (uint8_t cid);
 
-    /**
-     * @brief Get the valid lifetime.
-     * @return valid lifetime value
-     */
-    Time GetValidTime() const;
+  /**
+   * \brief Get the valid lifetime.
+   * \return valid lifetime value
+   */
+  Time GetValidTime () const;
 
-    /**
-     * @brief Set the valid lifetime.
-     * @param time the valid lifetime value
-     */
-    void SetValidTime(Time time);
+  /**
+   * \brief Set the valid lifetime.
+   * \param time the valid lifetime value
+   */
+  void SetValidTime (Time time);
 
-    /**
-     * @brief Set the last update time.
-     * @param time tthe last update time
-     */
-    void SetLastUpdateTime(Time time);
+  /**
+   * \brief Set the last update time.
+   * \param time tthe last update time
+   */
+  void SetLastUpdateTime (Time time);
 
-    /**
-     * @brief Get the last update time.
-     * @return the last update time
-     */
-    Time GetLastUpdateTime();
+  /**
+   * \brief Get the last update time.
+   * \return the last update time
+   */
+  Time GetLastUpdateTime ();
 
-    /**
-     * @brief Get the 6LoWPAN context prefix.
-     * @return context prefix value
-     */
-    Ipv6Prefix GetContextPrefix() const;
+  /**
+   * \brief Get the 6LoWPAN context prefix.
+   * \return context prefix value
+   */
+  Ipv6Prefix GetContextPrefix () const;
 
-    /**
-     * @brief Set the 6LoWPAN context prefix.
-     * @param context the context prefix value
-     */
-    void SetContextPrefix(Ipv6Prefix context);
+  /**
+   * \brief Set the 6LoWPAN context prefix.
+   * \param context the context prefix value
+   */
+  void SetContextPrefix (Ipv6Prefix context);
 
-    /**
-     * @brief Print the 6LoWPAN context.
-     * @param stream the ostream the 6LoWPAN context is printed to
-     */
-    void PrintContext(Ptr<OutputStreamWrapper> stream);
+  /**
+   * \brief Print the 6LoWPAN context.
+   * \param stream the ostream the 6LoWPAN context is printed to
+   */
+  void PrintContext (Ptr<OutputStreamWrapper> stream);
 
-  private:
-    /**
-     * @brief The compression flag, indicates that this context is valid for use in compression.
-     */
-    bool m_c;
+private:
+  /**
+   * \brief The compression flag, indicates that this context is valid for use in compression.
+   */
+  bool m_c;
 
-    /**
-     * @brief The context identifier value.
-     */
-    uint8_t m_cid;
+  /**
+   * \brief The context identifier value.
+   */
+  uint8_t m_cid;
 
-    /**
-     * @brief The valid lifetime value.
-     */
-    Time m_validTime;
+  /**
+   * \brief The valid lifetime value.
+   */
+  Time m_validTime;
 
-    /**
-     * @brief The context last update time.
-     */
-    Time m_lastUpdateTime;
+  /**
+   * \brief The context last update time.
+   */
+  Time m_lastUpdateTime;
 
-    /**
-     * @brief The context prefix value.
-     */
-    Ipv6Prefix m_context;
+  /**
+   * \brief The context prefix value.
+   */
+  Ipv6Prefix m_context;
 };
 
 } /* namespace ns3 */
--- src/sixlowpan/model/sixlowpan-nd-prefix.h
+++ src/sixlowpan/model/sixlowpan-nd-prefix.h
@@ -1,3 +1,4 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2015 Università di Firenze, Italy
  *
@@ -22,116 +23,116 @@
 #ifndef SIXLOW_ND_PREFIX_H
 #define SIXLOW_ND_PREFIX_H
 
-#include "ns3/ipv6-address.h"
-#include "ns3/nstime.h"
-#include "ns3/output-stream-wrapper.h"
-#include "ns3/simple-ref-count.h"
-
 #include <stdint.h>
+
+#include "ns3/nstime.h"
+#include "ns3/ipv6-address.h"
+#include "ns3/simple-ref-count.h"
+#include "ns3/output-stream-wrapper.h"
 
 namespace ns3
 {
 
 /**
- * @ingroup sixlowpan
- * @brief Router prefix container for 6LoWPAN ND.
+ * \ingroup sixlowpan
+ * \brief Router prefix container for 6LoWPAN ND.
  */
 class SixLowPanNdPrefix : public SimpleRefCount<SixLowPanNdPrefix>
 {
-  public:
-    /**
-     * @brief Constructor.
-     */
-    SixLowPanNdPrefix();
+public:
+  /**
+   * \brief Constructor.
+   */
+  SixLowPanNdPrefix ();
 
-    /**
-     * @brief Constructor.
-     * @param prefix network prefix advertised
-     * @param prefixLen prefix length ( 0 < x <= 128)
-     * @param prefTime preferred life time (default 7 days)
-     * @param validTime valid life time (default 30 days)
-     */
-    SixLowPanNdPrefix(Ipv6Address prefix, uint8_t prefixLen, Time prefTime, Time validTime);
+  /**
+   * \brief Constructor.
+   * \param prefix network prefix advertised
+   * \param prefixLen prefix length ( 0 < x <= 128)
+   * \param prefTime preferred life time (default 7 days)
+   * \param validTime valid life time (default 30 days)
+   */
+  SixLowPanNdPrefix (Ipv6Address prefix, uint8_t prefixLen, Time prefTime, Time validTime);
 
-    /**
-     * @brief Destructor.
-     */
-    ~SixLowPanNdPrefix();
+  /**
+   * \brief Destructor.
+   */
+  ~SixLowPanNdPrefix ();
 
-    /**
-     * @brief Get network prefix.
-     * @return network prefix
-     */
-    Ipv6Address GetPrefix() const;
+  /**
+   * \brief Get network prefix.
+   * \return network prefix
+   */
+  Ipv6Address GetPrefix () const;
 
-    /**
-     * @brief Set network prefix.
-     * @param prefix network prefix
-     */
-    void SetPrefix(Ipv6Address prefix);
+  /**
+   * \brief Set network prefix.
+   * \param prefix network prefix
+   */
+  void SetPrefix (Ipv6Address prefix);
 
-    /**
-     * @brief Get prefix length.
-     * @return prefix length
-     */
-    uint8_t GetPrefixLength() const;
+  /**
+   * \brief Get prefix length.
+   * \return prefix length
+   */
+  uint8_t GetPrefixLength () const;
 
-    /**
-     * @brief Set prefix length.
-     * @param prefixLen prefix length
-     */
-    void SetPrefixLength(uint8_t prefixLen);
+  /**
+   * \brief Set prefix length.
+   * \param prefixLen prefix length
+   */
+  void SetPrefixLength (uint8_t prefixLen);
 
-    /**
-     * @brief Get preferred lifetime.
-     * @return lifetime
-     */
-    Time GetPreferredLifeTime() const;
+  /**
+   * \brief Get preferred lifetime.
+   * \return lifetime
+   */
+  Time GetPreferredLifeTime () const;
 
-    /**
-     * @brief Set preferred lifetime.
-     * @param prefTime lifetime
-     */
-    void SetPreferredLifeTime(Time prefTime);
+  /**
+   * \brief Set preferred lifetime.
+   * \param prefTime lifetime
+   */
+  void SetPreferredLifeTime (Time prefTime);
 
-    /**
-     * @brief Get valid lifetime.
-     * @return lifetime
-     */
-    Time GetValidLifeTime() const;
+  /**
+   * \brief Get valid lifetime.
+   * \return lifetime
+   */
+  Time GetValidLifeTime () const;
 
-    /**
-     * @brief Set valid lifetime.
-     * @param validTime lifetime
-     */
-    void SetValidLifeTime(Time validTime);
+  /**
+   * \brief Set valid lifetime.
+   * \param validTime lifetime
+   */
+  void SetValidLifeTime (Time validTime);
 
-    /**
-     * @brief Print the prefix
-     * @param stream the ostream the prefix is printed to
-     */
-    void PrintPrefix(Ptr<OutputStreamWrapper> stream);
+  /**
+   * \brief Print the prefix
+   * \param stream the ostream the prefix is printed to
+   */
+  void PrintPrefix (Ptr<OutputStreamWrapper> stream);
 
-  private:
-    /**
-     * @brief Network prefix.
-     */
-    Ipv6Address m_prefix;
+private:
+  /**
+   * \brief Network prefix.
+   */
+  Ipv6Address m_prefix;
 
-    /**
-     * @brief Prefix length.
-     */
-    uint8_t m_prefixLength;
+  /**
+   * \brief Prefix length.
+   */
+  uint8_t m_prefixLength;
 
-    /**
-     * @brief Preferred time.
-     */
-    Time m_preferredLifeTime;
+  /**
+   * \brief Preferred time.
+   */
+  Time m_preferredLifeTime;
 
-    /**
-     * @brief Valid time.
-     */
-    Time m_validLifeTime;
+  /**
+   * \brief Valid time.
+   */
+  Time m_validLifeTime;
 };
 
 } /* namespace ns3 */
