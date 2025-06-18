3ebb20f87fb19a66bfdc1376e39c7fd8461ed01e
shaanzie <ishaanlagwankar@gmail.com>
2025-06-18 15:34:42 -0400

    Implemented switch clock

---
+++ src/core/model/replay-switch-clock.h
@@ -0,0 +1,39 @@
+/* 
+ * Copyright (c) 2023 Ishaan Lagwankar <lagwanka@msu.edu>
+ * 
+ * This file is part of the ns-3 network simulator.
+ * 
+ * ns-3 is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+ * 
+ * ns-3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with ns-3; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef REPLAY_SWITCH_CLOCK_H
+#define REPLAY_SWITCH_CLOCK_H
+
+#include "ns3/replay-clock.h"
+#include "ns3/log.h"
+
+namespace ns3 {
+
+class ReplaySwitchClock {
+public:
+    ReplaySwitchClock();
+    virtual ~ReplaySwitchClock();
+
+    void ReconcileClusterClock(ReplayClock clusterClock, int64_t c_nodeId, int64_t s_nodeId, int64_t physicalTime, int64_t u_epsilon, int64_t u_interval);
+
+private:
+    
+    // Add private member variables here
+    ReplayClock m_clusterClock;     //!< Cluster clock for the switch
+    ReplayClock m_switchClock;      //!< Switch clock for the switch
+
+    
+};
+
+} // namespace ns3
+
+#endif // REPLAY_SWITCH_CLOCK_H
