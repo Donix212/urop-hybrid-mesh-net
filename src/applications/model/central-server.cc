c69296e0f6c7f713469b2edfc0bc0d32c9be1eed
shaanzie <ishaanlagwankar@gmail.com>
2025-07-09 11:35:37 -0400

    Boilerplate code for central and radial process applications

---
+++ src/applications/model/central-server.cc
@@ -0,0 +1,129 @@
+/*
+ *  Copyright (c) 2007,2008,2009 INRIA, UDCAST
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Ishaan Lagwankar <lagwanka@msu.edu>
+ */
+
+#include "central-server.h"
+
+#include "packet-loss-counter.h"
+#include "seq-ts-header.h"
+
+#include "ns3/inet-socket-address.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/log.h"
+#include "ns3/nstime.h"
+#include "ns3/packet.h"
+#include "ns3/simulator.h"
+#include "ns3/socket-factory.h"
+#include "ns3/socket.h"
+#include "ns3/uinteger.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("CentralServer");
+
+NS_OBJECT_ENSURE_REGISTERED(CentralServer);
+
+TypeId
+CentralServer::GetTypeId()
+{
+    static TypeId tid =
+        TypeId("ns3::CentralServer")
+            .SetParent<SinkApplication>()
+            .SetGroupName("Applications")
+            .AddConstructor<CentralServer>();
+    return tid;
+}
+
+CentralServer::CentralServer()
+    : SinkApplication(DEFAULT_PORT),
+      m_socket{nullptr},
+      m_received{0}
+{
+    NS_LOG_FUNCTION(this);
+}
+
+CentralServer::~CentralServer()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+uint64_t
+CentralServer::GetReceived() const
+{
+    NS_LOG_FUNCTION(this);
+    return m_received;
+}
+
+void
+CentralServer::StartApplication()
+{
+    NS_LOG_FUNCTION(this);
+
+    if (!m_socket)
+    {
+        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
+        m_socket = Socket::CreateSocket(GetNode(), tid);
+        auto local = m_local;
+        if (local.IsInvalid())
+        {
+            local = InetSocketAddress(Ipv4Address::GetAny(), m_port);
+            NS_LOG_INFO(this << " Binding on port " << m_port << " / " << local << ".");
+        }
+        else if (InetSocketAddress::IsMatchingType(m_local))
+        {
+            const auto ipv4 = InetSocketAddress::ConvertFrom(m_local).GetIpv4();
+            NS_LOG_INFO(this << " Binding on " << ipv4 << " port " << m_port << " / " << m_local
+                                << ".");
+        }
+            
+        if (m_socket->Bind(local) == -1)
+        {
+            NS_FATAL_ERROR("Failed to bind socket");
+        }
+        m_socket->SetRecvCallback(MakeCallback(&UdpServer::HandleRead, this));
+    }
+}
+
+void
+CentralServer::StopApplication()
+{
+    NS_LOG_FUNCTION(this);
+
+    if (m_socket)
+    {
+        m_socket->SetRecvCallback(MakeNullCallback<void, Ptr<Socket>>());
+    }
+}
+
+void
+CentralServer::HandleRead(Ptr<Socket> socket)
+{
+    NS_LOG_FUNCTION(this << socket);
+    Address from;
+    while (auto packet = socket->RecvFrom(from))
+    {
+        Address localAddress;
+        socket->GetSockName(localAddress);
+        if (packet->GetSize() > 0)
+        {
+            const auto receivedSize = packet->GetSize();
+            if (InetSocketAddress::IsMatchingType(from))
+            {
+                NS_LOG_INFO("TraceDelay: RX " << receivedSize << " bytes from "
+                                              << InetSocketAddress::ConvertFrom(from).GetIpv4()
+                                              << " Sequence Number: " << currentSequenceNumber
+                                              << " Uid: " << packet->GetUid() << " TXtime: "
+                                              << seqTs.GetTs() << " RXtime: " << Simulator::Now()
+                                              << " Delay: " << Simulator::Now() - seqTs.GetTs());
+            }
+            m_received++;
+        }
+    }
+}
+
+} // Namespace ns3
---
+++ src/applications/model/central-server.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2007,2008,2009 INRIA, UDCAST
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Ishaan Lagwankar <lagwanka@msu.edu>
+ *
+ */
+
+#ifndef CENTRAL_SERVER_H
+#define CENTRAL_SERVER_H
+
+#include "sink-application.h"
+
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+namespace ns3
+{
+/**
+ * @ingroup applications
+ * @defgroup radialclientserver RadialClientServer
+ */
+
+/**
+ * @ingroup RadialClientServer
+ *
+ * @brief A radial server, receives UDP packets from a remote host.
+ *
+ *  UDP packets carry a 32bits sequence number followed by a 64bits time
+ *  stamp in their payloads. The application uses the sequence number
+ *  to determine if a packet is lost, and the time stamp to compute the delay.
+ */
+class CentralServer : public SinkApplication
+{
+  public:
+    static constexpr uint16_t DEFAULT_PORT{100}; //!< default port
+
+    /**
+     * @brief Get the type ID.
+     * @return the object TypeId
+     */
+    static TypeId GetTypeId();
+
+    CentralServer();
+    ~CentralServer() override;
+
+    /**
+     * @brief Returns the number of received packets
+     * @return the number of received packets
+     */
+    uint64_t GetReceived() const;
+
+  private:
+    void StartApplication() override;
+    void StopApplication() override;
+
+    /**
+     * @brief Handle a packet reception.
+     *
+     * This function is called by lower layers.
+     *
+     * @param socket the socket the packet was received to.
+     */
+    void HandleRead(Ptr<Socket> socket);
+
+    Ptr<Socket> m_socket;            //!< Socket
+    uint64_t m_received;             //!< Number of received packets
+};
+
+} // namespace ns3
+
+#endif /* CENTRAL_SERVER_H */
