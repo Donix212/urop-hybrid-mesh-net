a108d48722d466b3dc42bec538febed1d743c04f
Boh Jie Qi <jieqiboh5836@gmail.com>
2025-07-23 04:18:02 +0800

    sixlowpan: Update sixlowpan-nd-basic test, remove ping-test

--- src/sixlowpan/examples/sixlowpan-nd-basic-test.cc
+++ src/sixlowpan/examples/sixlowpan-nd-basic-test.cc
@@ -21,9 +21,9 @@ using namespace ns3;
 int
 main()
 {
-    // Create one 6LBR and one 6LN
+    // Create 1 6LBR and 4 6LNs
     NodeContainer nodes;
-    nodes.Create(2);
+    nodes.Create(5); // node 0 = 6LBR, node 1-4 = 6LNs
 
     // Set constant positions
     MobilityHelper mobility;
@@ -47,25 +47,38 @@ main()
     // Node 0 = 6LBR
     sixlowpan.InstallSixLowPanNdBorderRouter(devices.Get(0), "2001::");
     sixlowpan.SetAdvertisedPrefix(devices.Get(0), Ipv6Prefix("2001::", 64));
-    // Node 1 = 6LN
-    sixlowpan.InstallSixLowPanNdNode(devices.Get(1));
 
-    // Set up ping from 6LN to 6LBR (link-local address used)
-    PingHelper ping6;
-    ping6.SetAttribute("Count", UintegerValue(1));
-    ping6.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-    ping6.SetAttribute("Size", UintegerValue(16));
-    ping6.SetAttribute("Destination", AddressValue(Ipv6Address("fe80::ff:fe00:1")));      // 6LBR
-    ping6.SetAttribute("InterfaceAddress", AddressValue(Ipv6Address("fe80::ff:fe00:2"))); // 6LN
+    // Nodes 1-4 = 6LNs
+    for (uint32_t i = 1; i <= 4; ++i)
+    {
+        sixlowpan.InstallSixLowPanNdNode(devices.Get(i));
+    }
 
-    ApplicationContainer apps = ping6.Install(nodes.Get(1)); // 6LN is sender
-    apps.Start(Seconds(2.0));
-    apps.Stop(Seconds(10.0));
+    // Set up ping from each 6LN to 6LBR (link-local address of 6LBR is fe80::ff:fe00:1)
+    for (uint32_t i = 1; i <= 4; ++i)
+    {
+        std::ostringstream oss;
+        oss << "fe80::ff:fe00:" << std::hex << (i + 1); // 6LN link-local address
+
+        PingHelper ping6;
+        ping6.SetAttribute("Count", UintegerValue(1));
+        ping6.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+        ping6.SetAttribute("Size", UintegerValue(16));
+        ping6.SetAttribute("Destination", AddressValue(Ipv6Address("fe80::ff:fe00:1"))); // 6LBR
+        ping6.SetAttribute("InterfaceAddress", AddressValue(Ipv6Address(oss.str().c_str()))); // 6LN
+
+        ApplicationContainer apps = ping6.Install(nodes.Get(i));
+        Time startTime = Seconds(15.0); // wait for all nodes to finish registration
+        Time stopTime = Seconds(20.0);
+
+        apps.Start(startTime);
+        apps.Stop(stopTime);
+    }
 
     AsciiTraceHelper ascii;
-    lrWpanHelper.EnablePcapAll(std::string("ping-test"), true);
+    lrWpanHelper.EnablePcapAll(std::string("sixlowpan-nd-basic-test"), true);
 
-    Simulator::Stop(Seconds(12.0));
+    Simulator::Stop(Seconds(20.0));
     Simulator::Run();
     Simulator::Destroy();
     return 0;
