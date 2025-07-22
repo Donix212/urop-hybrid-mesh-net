e726dc82c6cf169a1cd94b9d06fc1cf2f039db0c
Boh Jie Qi <jieqiboh5836@gmail.com>
2025-07-23 04:18:01 +0800

    sixlowpan-nd: Add assertions to sixlowpan-nd-nsna-test

--- src/sixlowpan/test/sixlowpan-nd-nsna-test.cc
+++ src/sixlowpan/test/sixlowpan-nd-nsna-test.cc
@@ -44,78 +44,24 @@ class SixLowPanNdNsNaTest : public TestCase
     {
     }
 
-    struct SixLowPanPair
-    {
-        Ptr<Node> lbrNode;
-        Ptr<Node> lnNode;
-        Ptr<SixLowPanNetDevice> lbrDevice;
-        Ptr<SixLowPanNetDevice> lnDevice;
-        Ptr<SixLowPanNdProtocol> lbrNd;
-        Ptr<SixLowPanNdProtocol> lnNd;
-    };
-
-    SixLowPanPair SetupBasic6Ln6LbrPair()
-    {
-        SixLowPanPair result;
-
-        // Create nodes
-        NodeContainer nodes;
-        nodes.Create(2);
-        result.lbrNode = nodes.Get(0);
-        result.lnNode = nodes.Get(1);
-
-        // Set constant positions
-        MobilityHelper mobility;
-        mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-        mobility.Install(nodes);
-
-        // Install LrWpanNetDevices
-        LrWpanHelper lrWpanHelper;
-        NetDeviceContainer lrwpanDevices = lrWpanHelper.Install(nodes);
-        lrWpanHelper.CreateAssociatedPan(lrwpanDevices, 0);
-
-        // Install Internet stack
-        InternetStackHelper internetv6;
-        internetv6.Install(nodes);
-
-        // Install 6LoWPAN on top of LrWpan
-        SixLowPanHelper sixlowpan;
-        NetDeviceContainer devices = sixlowpan.Install(lrwpanDevices);
-
-        // Configure 6LoWPAN ND
-        // Node 0 = 6LBR
-        sixlowpan.InstallSixLowPanNdBorderRouter(devices.Get(0), "2001::");
-        sixlowpan.SetAdvertisedPrefix(devices.Get(0), Ipv6Prefix("2001::", 64));
-        // Node 1 = 6LN
-        sixlowpan.InstallSixLowPanNdNode(devices.Get(1));
-
-        // Output
-        result.lbrDevice = DynamicCast<SixLowPanNetDevice>(devices.Get(0));
-        result.lnDevice = DynamicCast<SixLowPanNetDevice>(devices.Get(1));
-        result.lbrNd = result.lbrNode->GetObject<SixLowPanNdProtocol>();
-        result.lnNd = result.lnNode->GetObject<SixLowPanNdProtocol>();
-
-        return result;
-    }
-
     void DoRun() override
     {
         // LogComponentEnable ("SixLowPanNetDevice", LOG_LEVEL_FUNCTION);
         // LogComponentEnable ("SixLowPanNdProtocol", LOG_LEVEL_FUNCTION);
-        LogComponentEnable("SixLowPanNdProtocol", LOG_LEVEL_INFO);
+        // LogComponentEnable("SixLowPanNdProtocol", LOG_LEVEL_INFO);
 
         // Disable sending multicast RS by commenting out SixLowPanNdProtocol::FunctionDadTimeout
         // Config::SetDefault ("ns3::SixLowPanNetDevice::UseMeshUnder", BooleanValue (true));
-
-        // 6LN - node 1
-        // LLaddr: fe80::ff:fe00:2
-        // Link-layer address: 02:00:00:00:00:02
-        // Gaddr: 2001::ff:fe00:2 (Needs reg first)
 
         // 6LBR - node 0
         // LLaddr: fe80::ff:fe00:1
         // Link-layer address: 02:00:00:00:00:01
         // Gaddr: 2001::ff:fe00:1
+
+        // 6LN - node 1
+        // LLaddr: fe80::ff:fe00:2
+        // Link-layer address: 02:00:00:00:00:02
+        // Gaddr: 2001::ff:fe00:2 (Needs reg first)
 
         // Basic Exchange, then assert NC, 6LNC and RT contents of 6LN and 6LBR
         // Create nodes
@@ -149,21 +95,51 @@ class SixLowPanNdNsNaTest : public TestCase
         // Node 1 = 6LN
         sixlowpan.InstallSixLowPanNdNode(devices.Get(1));
 
-        // std::ostringstream stringStream1v6;
-        // Ptr<OutputStreamWrapper> ndiscStream = Create<OutputStreamWrapper>(&stringStream1v6);
+        // Ptr<OutputStreamWrapper> outputStream = Create<OutputStreamWrapper>(&std::cout);
+        // Ipv6RoutingHelper::PrintNeighborCacheAllEvery(Seconds(1), outputStream);
+        // Ipv6RoutingHelper::PrintRoutingTableAllEvery(Seconds(1), outputStream);
 
-        // Ipv6RoutingHelper::PrintNeighborCacheAllAt(Seconds(50), std::cout);
-        // Ipv6RoutingHelper::PrintRoutingTableAllAt
-        Ptr<OutputStreamWrapper> neighborStream = Create<OutputStreamWrapper>(&std::cout);
+        std::ostringstream ndiscStream;
+        Ptr<OutputStreamWrapper> outputNdiscStream = Create<OutputStreamWrapper>(&ndiscStream);
+        std::ostringstream routingTableStream;
+        Ptr<OutputStreamWrapper> outputRoutingTableStream =
+            Create<OutputStreamWrapper>(&routingTableStream);
 
-        // Print
-        Ipv6RoutingHelper::PrintNeighborCacheAllEvery(Seconds(1), neighborStream);
-        Ipv6RoutingHelper::PrintRoutingTableAllEvery(Seconds(1), neighborStream);
-        Simulator::Stop(Seconds(50.0));
+        Ipv6RoutingHelper::PrintNeighborCacheAllAt(Seconds(5), outputNdiscStream);
+        Ipv6RoutingHelper::PrintRoutingTableAllAt(Seconds(5), outputRoutingTableStream);
+
+        Simulator::Stop(Seconds(5.0));
         Simulator::Run();
         Simulator::Destroy();
 
-        // std::cout << stringStream1v6.str();
+        // Print the streams to console
+        // std::cout << "=== NDISC CACHE ===\n" << ndiscStream.str();
+        // std::cout << "=== ROUTING TABLE ===\n" << routingTableStream.str();
+
+        // Check if arp caches are populated correctly in the first channel
+        constexpr auto expectedNdiscStream =
+            "NDISC Cache of node 0 at time +5s\n"
+            "2001::ff:fe00:2 dev 2 lladdr 00-06-02:00:00:00:00:02 REACHABLE REGISTERED\n"
+            "fe80::ff:fe00:2 dev 2 lladdr 00-06-02:00:00:00:00:02 REACHABLE REGISTERED\n"
+            "NDISC Cache of node 1 at time +5s\n"
+            "fe80::ff:fe00:1 dev 2 lladdr 00-06-02:00:00:00:00:01 REACHABLE GARBAGE-COLLECTIBLE\n";
+        NS_TEST_EXPECT_MSG_EQ(ndiscStream.str(), expectedNdiscStream, "NdiscCache is incorrect.");
+
+        // Check if ndisc caches are populated correctly in the first channel
+        constexpr auto expectedRoutingTableStream =
+            "Node: 0, Time: +5s, Local time: +5s, Ipv6StaticRouting table\n"
+            "Destination                    Next Hop                   Flag Met Ref Use If\n"
+            "::1/128                        ::                         UH   0   -   -   0\n"
+            "fe80::/64                      ::                         U    0   -   -   1\n"
+            "2001::ff:fe00:2/128            fe80::ff:fe00:2            UH   0   -   -   1\n\n"
+            "Node: 1, Time: +5s, Local time: +5s, Ipv6StaticRouting table\n"
+            "Destination                    Next Hop                   Flag Met Ref Use If\n"
+            "::1/128                        ::                         UH   0   -   -   0\n"
+            "fe80::/64                      ::                         U    0   -   -   1\n"
+            "::/0                           fe80::ff:fe00:1            UG   0   -   -   1\n\n";
+        NS_TEST_EXPECT_MSG_EQ(routingTableStream.str(),
+                              expectedRoutingTableStream,
+                              "Routing table does not match expected.");
     }
 };
 
