f5daeb3483707163977be35f3c06f3a244a3396c
Boh Jie Qi <jieqiboh5836@gmail.com>
2025-08-03 02:08:53 +0800

    sixlowpan: refactor rovr tests to use simplenetdevice

--- src/sixlowpan/test/sixlowpan-nd-rovr-test.cc
+++ src/sixlowpan/test/sixlowpan-nd-rovr-test.cc
@@ -7,18 +7,12 @@
  */
 
 #include "ns3/core-module.h"
-#include "ns3/csma-helper.h"
-#include "ns3/inet6-socket-address.h"
-#include "ns3/internet-apps-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/internet-stack-helper.h"
-#include "ns3/lr-wpan-module.h"
-#include "ns3/mobility-module.h"
+#include "ns3/simple-net-device-helper.h"
 #include "ns3/simple-net-device.h"
 #include "ns3/sixlowpan-module.h"
-#include "ns3/sixlowpan-nd-prefix.h"
 #include "ns3/sixlowpan-nd-protocol.h"
-#include "ns3/socket.h"
 #include "ns3/test.h"
 
 #include <fstream>
@@ -49,15 +43,9 @@ class SixLowPanNdRovrTest : public TestCase
         Ptr<Node> lbrNode = nodes.Get(0);
         Ptr<Node> lnNode = nodes.Get(1);
 
-        // Set constant positions
-        MobilityHelper mobility;
-        mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-        mobility.Install(nodes);
-
-        // Install LrWpanNetDevices
-        LrWpanHelper lrWpanHelper;
-        NetDeviceContainer lrwpanDevices = lrWpanHelper.Install(nodes);
-        lrWpanHelper.CreateAssociatedPan(lrwpanDevices, 0);
+        // Install SimpleNetDevice instead of LrWpan
+        SimpleNetDeviceHelper simpleNetDeviceHelper;
+        NetDeviceContainer simpleNetDevices = simpleNetDeviceHelper.Install(nodes);
 
         // Install IPv6 stack
         InternetStackHelper internetv6;
@@ -65,7 +53,7 @@ class SixLowPanNdRovrTest : public TestCase
 
         // Install 6LoWPAN
         SixLowPanHelper sixlowpan;
-        NetDeviceContainer devices = sixlowpan.Install(lrwpanDevices);
+        NetDeviceContainer devices = sixlowpan.Install(simpleNetDevices);
 
         // Install 6LoWPAN-ND
         sixlowpan.InstallSixLowPanNdBorderRouter(devices.Get(0), "2001::");
@@ -73,15 +61,19 @@ class SixLowPanNdRovrTest : public TestCase
 
         sixlowpan.InstallSixLowPanNdNode(devices.Get(1)); // 6LN
 
-        // Set up trace connections BEFORE sending packets
-        Ptr<SixLowPanNdProtocol> lnNd = lnNode->GetObject<SixLowPanNdProtocol>();
-        lnNd->TraceConnectWithoutContext("NaRx",
-                                         MakeCallback(&SixLowPanNdRovrTest::NaRxSink, this));
+        Ptr<SixLowPanNdProtocol> lnNdProtocol = lnNode->GetObject<SixLowPanNdProtocol>();
+        lnNdProtocol->TraceConnectWithoutContext(
+            "NaRx",
+            MakeCallback(&SixLowPanNdRovrTest::NaRxSink, this));
+
+        // Get interface of 6LBR
+        Ptr<Ipv6L3Protocol> lbrIpv6 = lbrNode->GetObject<Ipv6L3Protocol>();
+        Ptr<Ipv6Interface> lbrIf = lbrIpv6->GetInterface(0)->GetObject<Ipv6Interface>();
+        Address lbrMac = simpleNetDevices.Get(0)->GetAddress(); // Get the MAC address of 6LBR
 
         // Construct NS (EARO) with same target LLaddr as 6LN, but different ROVR
-        Ipv6Address lnLLaddr("fe80::ff:fe00:2");
-        Ipv6Address lbrLLaddr("fe80::ff:fe00:1");
-        Address lbrMac = lrwpanDevices.Get(0)->GetAddress();
+        Ipv6Address lnLLaddr("fe80::200:ff:fe00:2");
+        Ipv6Address lbrLLaddr("fe80::200:ff:fe00:1");
         std::vector<uint8_t> rovr(16, 0); // Different ROVR (all zeros)
         Ptr<NetDevice> lnSixDevice = devices.Get(1);
 
@@ -96,12 +88,6 @@ class SixLowPanNdRovrTest : public TestCase
                             0,          // TID
                             lnSixDevice // NetDevice to send from
         );
-
-        // Print tables
-        Ptr<OutputStreamWrapper> out1 = Create<OutputStreamWrapper>(&std::cout);
-        Ipv6RoutingHelper::PrintNeighborCacheAllAt(Seconds(7), out1);
-
-        lrWpanHelper.EnablePcapAll(std::string("sixlowpan-nd-rovr-test"), true);
 
         Simulator::Stop(Seconds(10));
         Simulator::Run();
@@ -132,8 +118,6 @@ class SixLowPanNdRovrTest : public TestCase
             NS_TEST_ASSERT_MSG_EQ(earo.GetStatus(),
                                   static_cast<uint8_t>(1),
                                   "NA status should be 1 (duplicate address)");
-
-            std::cout << "ROVR conflict test passed: NA received with correct status" << std::endl;
         }
     }
 
